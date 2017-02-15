5989440d4b26da8d2115b43ccaad876e53bba2e3
pliva <pliva@avast.com>
2017-02-15 21:10:59 +0100

    travis

--- build.sh
+++ build.sh
@@ -11,19 +11,9 @@ fi
 
 dotnet restore
 
-# Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
-# but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
-
-dotnet test ./src/zonky-client-test -c Release -f netcoreapp1.0
-
-# Instead, run directly with mono for the full .net version 
-dotnet build ./src/zonky-client -c Release -f net451
-
-# mono \  
-# ./test/TEST_PROJECT_NAME/bin/Release/net451/*/dotnet-test-xunit.exe \
-# ./test/TEST_PROJECT_NAME/bin/Release/net451/*/TEST_PROJECT_NAME.dll
+dotnet build src/zonky-client
+dotnet build src/zonky-client-test
+dotnet test src/zonky-client-test
 
 revision=${TRAVIS_JOB_ID:=1}  
 revision=$(printf "%04d" $revision) 
-
-dotnet pack ./src/zonky-client -c Release -o ./artifacts --version-suffix=$revision  
